143bc82838a600b60a1e33cf11419d7c0d58dd7c
Jack Farley <or4e3dsw@bytes.nz>
2017-10-18 17:40:24 +0300

    Updated style and added weather link
    - Added link to weather page in header
    - Added colour to h2 and h3 headings
    - Added padding to body > main

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,12 @@ export const RDHeader = () => (
     <div className="nav" tabIndex="1">
       <div className="hamburger"></div>
       <nav>
-        <a href="//www.rd">Information</a>
-        <a href="//files.rd">Files</a>
-        <a href="//data.rd">Data</a>
-        <a href="//maps.rd">Maps</a>
-        <a href="//upload.rd">Upload</a>
+        <a href="//www.rd" title="Information about Reef Doctor">Information</a>
+        <a href="//files.rd" title="Useful files including all Reef Doctor presentations, useful software and sustaintable living (SL) data templates">Files</a>
+        <a href="//data.rd" title="Aquaculture and science data">Data</a>
+        <a href="//maps.rd" title="Maps and POI of the Bay of Ranobe">Maps</a>
+        <a href="//upload.rd" title="The place to upload completed SL data worksheets">Upload</a>
+        <a href="//www.rd/weather.html" title="Show the current weather forecase">Weather</a>
       </nav>
     </div>
     <div className="logo-text"></div>
